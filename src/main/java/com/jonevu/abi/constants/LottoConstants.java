38fdc2b31e6a8dcb6b21ab281cac5e29eb61bd4c
john-evbuoma <jonevu@comcast.net>
2023-12-17 15:56:42 -0600

    added dot comment

--- src/main/java/com/jonevu/abi/constants/LottoConstants.java
+++ src/main/java/com/jonevu/abi/constants/LottoConstants.java
@@ -9,19 +9,19 @@ package com.jonevu.abi.constants;
  */
 public final class LottoConstants {
 
-    // ... POWER BALL ...
+    // ... POWER BALL ....
     public final static int PWR_BALL_PICK5_MAX_NUM = 69;
     public final static int PWR_BALL_MAX_NUM = 26;
     public final static String PWR_BALL = "PB";
     public final static String POWERBALL = "POWERBALL";
     
-    // ... MEGA ...
+    // ... MEGA ....
     public final static int MEGA_PICK5_MAX_NUM = 70;
     public final static int MEGA_PWR_BALL_MAX_NUM = 25; 
     public final static String MEGA = "M";
     public final static String MEGGA = "MEGA";
     
-    // ... LOTTO ...
+    // ... LOTTO ....
     public final static int LOTTO_PICKS_MAX_NUM = 52;
     public final static String REG_LOTTO = "RL";
     public final static String LOTTO = "LOTTO";
